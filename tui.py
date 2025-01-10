eee44d1556782ba85c5289bc7878567f8ca3f7af
senyagithub <knight.fo@yandex.ru>
2025-01-10 16:15:53 +0000

    options func

--- tui.py
+++ tui.py
@@ -11,3 +11,23 @@ def welcome(width=100):
 
     # Print the dashes below
     print(dashes)
+
+
+def main_menu():
+    while True:
+        try:
+            option = int(input('''
+    Main Menu:
+      [1] Load Data
+      [2] Process Data
+      [3] Visualise Data
+      [4] Exit
+    Select option:
+    '''))
+            if option in range(1,5):
+                return option
+            else:
+                print("Choose an option between 1 and 4. ")
+        except Exception as e:
+            error("please enter a number.")
+            pass
